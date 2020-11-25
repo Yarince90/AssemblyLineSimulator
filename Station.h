0c396b7e39ba55054926fb754f5e83ef5b01e540
Yarince Vigil Teves <59423827+Yarince90@users.noreply.github.com>
2020-11-25 13:36:59 -0500

    Update Station.h

--- Station.h
+++ Station.h
@@ -1,15 +1,20 @@
 #pragma once
-/*
-Name: Yarince Vigil
-Email: yvigilteves@myseneca.ca
-Student ID: yvigilteves
-Student Number: 016261125
-Date Completed: 11/17/2020
-
-I have done all the coding by myself
-and only copied the code that my professor
-provided to complete my workshops and assignments.
-*/
+/* Citation and Sources...
+OOP345 Final Project
+Module: Station
+Filename: Station.h
+Version 1.0
+Author Yarince Vigil
+contact@yarince.com
+-----------------------------------------------------------
+Date: 2020/11/17
+Preliminary release: 11/17/2020
+Revised History:
+-First release
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code
+that my professor provided to complete my workshops and assignments.
+-----------------------------------------------------------*/
 #include <string>
 
 typedef unsigned int uInt;
