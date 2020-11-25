bea5759bda29aefa436dbb1249cd3794529c5d7c
Yarince Vigil Teves <59423827+Yarince90@users.noreply.github.com>
2020-11-25 13:35:49 -0500

    Update Station.cpp

--- Station.cpp
+++ Station.cpp
@@ -1,15 +1,20 @@
 #define _CRT_SECURE_NO_WARNINGS
-/*
-Name: Yarince Vigil
-Email: yvigilteves@myseneca.ca
-Student ID: yvigilteves
-Student Number: 016261125
-Date Completed: 11/17/2020
-
-I have done all the coding by myself
-and only copied the code that my professor
-provided to complete my workshops and assignments.
-*/
+/* Citation and Sources...
+OOP345 Final Project
+Module: Station
+Filename: Station.cpp
+Version 1.0
+Author Yarince Vigil
+contact@yarince.com
+-----------------------------------------------------------
+Date: 2020/11/17
+Preliminary release: 2020/11/17
+Revised History:
+-First release
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code
+that my professor provided to complete my workshops and assignments.
+-----------------------------------------------------------*/
 #include <iostream>
 #include <iomanip>
 #include <string>
